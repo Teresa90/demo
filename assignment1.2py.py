d7abac85083927d99cf42541788c728cc3a9b80b
Teresa90 <njerimacharia.nm93@gmail.com>
2021-10-19 14:46:25 +0300

    second assignment

---
+++ assignment1.2py.py
@@ -0,0 +1,23 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:\Users\TNMACHARIA\Desktop\Jenga Assingments\assisngment2.py ====
+This program computers the roots
+of a second order equation:
+
+    ax^2+bx+c=0  
+
+please enter a: 8
+please enter b: 5
+please enter c: 0
+
+The roots are : 
+xl =  0.0
+x2 =  -10.0
+
+ Thank you for using this program
+=================================
+***   All rights reserved     ***
+=================================
+
+>>> 
